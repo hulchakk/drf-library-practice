6874cdeeaa4c07e3ae8ed9e8fe8b935ecb885a2a
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 21:29:33 +0200

    make list less detailed

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import RetrieveModelMixin, ListModelMixin
 
-from borrowing_service.serializers import BorrowingSerializer
+from borrowing_service.serializers import BorrowingSerializer, BorrowingDetailSerializer
 from borrowing_service.models import Borrowing
 
 
 class BorrowingViewSet(GenericViewSet, RetrieveModelMixin, ListModelMixin):
     queryset = Borrowing.objects.all()
-    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        if self.action == "list":
+            return BorrowingSerializer
