22d931f29df88877a025a59679d06a1f48e70f23
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 21:04:19 +0200

    feat(borrowing_service): implement Borrowing model

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from book_service.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        related_name="borrowings",
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        get_user_model(),
+        related_name="borrowings",
+        on_delete=models.CASCADE
+    )
