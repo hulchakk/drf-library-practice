7b826142b0b236dc1b36401db84c4293fb43c00c
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 19:40:26 +0200

    feat(book_service): implement CRUD for Book and books endpoint

--- book_service/urls.py
+++ book_service/urls.py
@@ -1,7 +1,19 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from book_service.views import (
+    BookViewSet
+)
 
 
-urlpatterns = []
+router = DefaultRouter()
+
+router.register("books", BookViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls))
+]
 
 
 app_name = "book_service"
--- book_service/views.py
+++ book_service/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from book_service.models import (
+    Book,
+)
+from book_service.serializers import (
+    BookSerializer,
+)
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
