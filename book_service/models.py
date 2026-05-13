a2e624bac186ffbba8f913dd63e2a56268162654
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 19:16:20 +0200

    feat(book_service): add Book model

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+BOOK_COVER_CHOICES = {
+    "H": "HARD",
+    "S": "SOFT",
+}
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    Cover = models.CharField(
+        max_length=4,
+        choices=BOOK_COVER_CHOICES,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(decimal_places=2, max_digits=7)
