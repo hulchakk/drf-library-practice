9539391286ab85060595a23d1790a3855bb2c53b
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 19:19:41 +0200

    feat(book_service): add str method for Book model

--- book_service/models.py
+++ book_service/models.py
@@ -16,3 +16,6 @@ class Book(models.Model):
     )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(decimal_places=2, max_digits=7)
+
+    def __str__(self):
+        return f"{self.title} ({self.author})"
