219f279a4daf21eda978cf1d04a89554b5b9438f
Dmytro <ys33ys33ys55@gmail.com>
2026-05-13 19:01:51 +0200

    initialize empty django app named book_service

--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "book_service",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
