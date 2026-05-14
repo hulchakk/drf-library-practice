bf81880a131f8c4f7be09771f86a5fb52c3ff5fb
Dmytro <ys33ys33ys55@gmail.com>
2026-05-14 08:14:07 +0200

    implement user_id filter for admin users and is_active filter for evryone

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -10,7 +10,22 @@ class BorrowingViewSet(ModelViewSet):
     queryset = Borrowing.objects.all()
 
     def get_queryset(self):
-        queryset = self.queryset.filter(user=self.request.user)
+        queryset = self.queryset
+        is_active = self.request.query_params.get("is_active")
+
+        if is_active == "true":
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        elif is_active == "false":
+            queryset = queryset.filter(actual_return_date__isnull=False)
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
         return queryset
 
     def get_serializer_class(self):
